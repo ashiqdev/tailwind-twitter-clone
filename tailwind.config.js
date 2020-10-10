3d9b9b710c85fcd47ec743ce8fad4cc36da52bff
Ashiqur Rahman <ashiquejava@gmail.com>
2020-10-10 13:07:47 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883"
+        }
+      },
+      height: {
+        112: "28rem"
+      },
+      width: {
+        "1/8": "12.5%",
+        "7/8": "87.5%"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
